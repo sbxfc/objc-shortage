35cc1d015ae3106388b8d58b279c7129399a5c0d
truthbeauty <303040805@qq.com>
2017-02-09 19:51:09 +0800

    Commit.

---
+++ AssetCatalog/AssetCatalog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AssetCatalog
+//
+//  Created by sbxfc on 17/2/9.
+//  Copyright © 2017年 site.ok2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
